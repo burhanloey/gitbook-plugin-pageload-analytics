724264f0b43bd500431d1dff37422d538055dcfa
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-06 15:23:26 +0800

    Move code into js asset

---
+++ assets/track.js
@@ -0,0 +1,5 @@
+require(['gitbook', 'jquery'], function(gitbook, $) {
+    gitbook.events.on('page.change', function() {
+        console.log("Hello! We're on new page now.");
+    });
+});
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
-    hooks: {}
+    book: {
+        assets: './assets',
+        js: [
+            'track.js'
+        ]
+    }
 };
