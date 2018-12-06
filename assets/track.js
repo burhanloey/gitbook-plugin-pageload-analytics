4c1318cef79ccd354637decc0ec6b0a8161346e0
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-06 18:10:17 +0800

    Set page to current page

--- assets/track.js
+++ assets/track.js
@@ -3,6 +3,7 @@ require([
     'jquery'
 ], function(gitbook, $) {
     gitbook.events.on('page.change', function() {
+        ga('set', 'page', window.location.pathname);
         ga(function(tracker) {
             console.log('tracker= ' + JSON.stringify(tracker));
         });
