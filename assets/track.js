6b798329df22a5f6a6f2ca23f43aa1d6832159b8
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-06 17:26:05 +0800

    Create tracker at template

--- assets/track.js
+++ assets/track.js
@@ -2,16 +2,6 @@ require([
     'gitbook',
     'jquery'
 ], function(gitbook, $) {
-    gitbook.events.on('start', function() {
-        var token = gitbook
-            .state
-            .config
-            .pluginsConfig["pageload-analytics"]
-            .token;
-
-        console.log("token= " + token);
-    });
-
     gitbook.events.on('page.change', function() {
         console.log("Hello! We're on new page now.");
     });
