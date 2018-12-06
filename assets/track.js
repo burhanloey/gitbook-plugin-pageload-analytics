6ad0921f8dd2a00c7f87bbdee9e2483ce98a969e
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-06 18:20:28 +0800

    Finalize page change event

--- assets/track.js
+++ assets/track.js
@@ -4,8 +4,6 @@ require([
 ], function(gitbook, $) {
     gitbook.events.on('page.change', function() {
         ga('set', 'location', window.location.href);
-        ga(function(tracker) {
-            console.log('tracker= ' + JSON.stringify(tracker));
-        });
+        ga('send', 'pageview');
     });
 });
